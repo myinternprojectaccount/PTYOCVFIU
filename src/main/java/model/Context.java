8e290de0ebb71e3c3356f8e075d0a0af54661131
Volkan AVCI <ce.volkan.avci@gmail.com>
2015-03-14 18:08:14 +0200

    gol

--- src/main/java/model/Context.java
+++ src/main/java/model/Context.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Context {
-
+//hasim seni nasil ekleyecem bakalim
 	private int contextId;
 	private int columnNo;
 	private List<String> data;
